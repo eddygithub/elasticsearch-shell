33286c59ff3172ec84663df16948c22caf9ab41d
Eddy Zhu <ezhu@dsny.nyc.gov>
2015-05-14 01:19:48 -0400

    added configuration path

--- src/main/java/com/akin/elasticsearch/tools/shell/ElasticSearchShellApplication.java
+++ src/main/java/com/akin/elasticsearch/tools/shell/ElasticSearchShellApplication.java
@@ -18,5 +18,4 @@ public class ElasticSearchShellApplication {
 		shell.promptLoop();
 		shell.waitForComplete();
 	}
-
 }
--- src/main/java/com/akin/elasticsearch/tools/shell/commands/ServerStartUpCommands.java
+++ src/main/java/com/akin/elasticsearch/tools/shell/commands/ServerStartUpCommands.java
@@ -4,6 +4,7 @@ import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 
 import java.util.Properties;
 
+import org.apache.commons.lang.StringUtils;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.ImmutableSettings;
@@ -22,9 +23,11 @@ public class ServerStartUpCommands implements CommandMarker{
 	private static final String COMMAND_STOP_NODE = "stop node";
 	private static final String COMMAND_LIST_NODE = "list node";
 	private static final String HELP_COMMAND_START_NODE = "This startup a node within elasticsearch cluster";
+	private static final String HELP_CLI_OPTION_CONF = "Configuration direction that contains elasticsearch.yml and logging.yml";
 	
 	private static final String CLUSTER_NAME = "cluster.name";
 	private static final String NODE_NAME = "node.name";
+	private static final String CONFIGURATION_PATH = "path.conf";
 	//TODO add some conditions
 //	@CliAvailabilityIndicator(value={"server-start"})
 //	public boolean isCommandAvailable(){
@@ -32,11 +35,16 @@ public class ServerStartUpCommands implements CommandMarker{
 //	}
 	
 	@CliCommand(help=HELP_COMMAND_START_NODE, value={COMMAND_START_NODE})
-	public String startUpNode(@CliOption(key="cluster-name", unspecifiedDefaultValue="es-cluster") String clusterName, 
-			@CliOption(key="node-name") String nodeName){
+	public String startUpNode(
+			@CliOption(key="cluster-name", mandatory=true, unspecifiedDefaultValue="es-cluster") String clusterName, 
+			@CliOption(key="node-name", mandatory=true) String nodeName,
+			@CliOption(key="config-dir", help=HELP_CLI_OPTION_CONF) String configDir){
 		Properties props = new Properties();
 		props.put(CLUSTER_NAME, clusterName);
 		props.put(NODE_NAME, nodeName);
+		if(!StringUtils.isEmpty(configDir)){
+			props.put(CONFIGURATION_PATH, configDir);
+		}
 		Settings settings = ImmutableSettings.settingsBuilder().put(props).build();
 		Node node = nodeBuilder().settings(settings).node();
 		String commandString = String.format("starting a new node: %s in cluster: %s", nodeName, clusterName);
